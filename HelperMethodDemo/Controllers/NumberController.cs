b4620519078434a92e5f62275a972e2657ee2371
SAHDEVSINH PARMAR <sahdevsinhparmar1753@gmail.com>
2024-12-01 15:04:01 +0530

    Add project files.

---
+++ HelperMethodDemo/Controllers/NumberController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelperMethodDemo.Models;
+
+namespace HelperMethodDemo.Controllers
+{
+    public class NumberController : Controller
+    {
+        // GET: Number
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MultiNumberModel()
+        {
+            return View(new Operation());
+        }
+        [HttpPost]
+        public ActionResult PerformModel(Operation operation)
+        {
+            switch (operation.Opr)
+            {
+                case "1":
+                    if (operation.IsFloating)
+                        ViewBag.Ans = "Addition = " + (Convert.ToSingle(operation.No1) + Convert.ToSingle(operation.No2));
+                    else
+                        ViewBag.Ans = "Addition = " + (operation.No1 + operation.No2);
+                    break;
+                case "2":
+                    if (operation.IsFloating)
+                        ViewBag.Ans = "Subtraction = " + (Convert.ToSingle(operation.No1) - Convert.ToSingle(operation.No2));
+                    else
+                        ViewBag.Ans = "Subtraction = " + (operation.No1 - operation.No2);
+                    break;
+                case "3":
+                    if (operation.IsFloating)
+                        ViewBag.Ans = "Multiplication = " + (Convert.ToSingle(operation.No1) * Convert.ToSingle(operation.No2));
+                    else
+                        ViewBag.Ans = "Multiplication = " + (operation.No1 * operation.No2);
+                    break;
+                case "4":
+                    if (operation.IsFloating)
+                        ViewBag.Ans = "Division = " + (Convert.ToSingle(operation.No1) / Convert.ToSingle(operation.No2));
+                    else
+                        ViewBag.Ans = "Division = " + (operation.No1 / operation.No2);
+                    break;
+                default:
+                    ViewBag.Ans = "Invalid Operation";
+                    break;
+
+            }
+            return View("MultiNumberModel",operation);
+        }
+    }
+}
---
+++ HelperMethodDemo/Models/Operation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelperMethodDemo.Models
+{
+    public class Operation
+    {
+        public int No1 { get; set; }
+        public int No2 { get; set; }
+        public string Opr { get; set; }
+        public bool IsFloating { get; set; }
+    }
+}
